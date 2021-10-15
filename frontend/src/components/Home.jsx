3546868d3528be4937daffb3688f688f7a36b542
itsmetherishabh <rishabhjainsept@gmail.com>
2021-10-15 20:09:05 +0530

    Home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
+import React from "react";
+import logo from "../images/degree_logo.png";
 
 const Home = () => {
+  return (
+    <div>
+      <h1 style={{ fontFamily: "Akronim", backgroundColor: "#99ffff" }}>
+        <marquee>
+          <img src={logo} alt='' />
+          Reasearch on Learning post School & Scope
+        </marquee>
+      </h1>
 
-
-    return (
-        <div>
-            <h1 style={{ fontFamily: 'Akronim' }}>Reasearch on Learning post School & Scope</h1>
-            <div className="card" style={{width: "18rem"}}>
-                <img src="science.png" className="card-img-top" alt="science.png" />
-                <div className="card-body">
-                    <h5 className="card-title">rlss</h5>
-                    <p className="card-text">best career website</p>
-                </div>
+      <section className='header'>
+        <div className='text-box'>
+          <h1>world's Best Website</h1>
+          <p>
+            <h2>How We Will Help You I</h2>
+          </p>
+        </div>
+      </section>
+      <section className='testimonials'>
+        <h1>What Our Students Says</h1>
+        <div className='row'>
+          <div className='testimonial-col'>
+            <img src='image/user1.jpg' alt='' />
+            <div>
+              <p>Lorem ipsum dolor sit amet copp</p>
+              <h3>kartik singh</h3>
             </div>
+          </div>
+          <div className='testimonial-col'>
+            <img src='image/user1.jpg' alt='' />
+            <div>
+              <p>Lorem ipsum dolor sit amet copp</p>
+              <h3>Nitin Sharma</h3>
             </div>
-            )
-    }
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-
-            export default Home
+export default Home;
