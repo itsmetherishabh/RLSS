ff74052ffe0a2cb94104e0e3d242de2bd7c24283
itsmetherishabh <rishabhjainsept@gmail.com>
2021-11-15 00:27:57 +0530

    basics of home page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
+        // <div className="container-fluid text-light p-3"  style={{background:"#212121"}}>
+        //     &copy; COPYRIGHT 2021 RLSS
+        // </div>
         <div className="container-fluid text-light p-3"  style={{background:"#212121"}}>
-            &copy; COPYRIGHT 2021 RLSS
+            <div className="row">
+                <div className="col-3"></div>
+                <div className="col-3"></div>
+                <div className="col-3"></div>
+                <div className="col-3">
+                    <Link to="/contact">Contact Us</Link>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4">&copy; COPYRIGHT 2021 RLSS</div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Footer
